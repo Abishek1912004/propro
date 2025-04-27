373e9442fc06f601c18dc208d841e0c46549facb
Abishek1912004 <21cse035.abishek@gmail.com>
2025-04-28 00:15:56 +0530

    add

--- palindrome.java
+++ palindrome.java
@@ -20,5 +20,29 @@ class palindrome
     {
         int c=n+n;
        System.out.println(c);
+    }import java.util.*;
+class Palindrome
+{
+    public static void main(String []args)
+    {
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter a number: ");
+       int n = sc.nextInt();
+       sc.close();
+       multi(n);
+       add(n);
     }
+
+    public static void multi(int n)
+    {
+        int result = n * n;
+        System.out.println("Square of " + n + " is: " + result);
+    }
+
+    public static void add(int n)
+    {
+        int result = n + n;
+        System.out.println("Double of " + n + " is: " + result);
+    }
+}
 }
