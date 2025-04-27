f9a552a8a2becca6e30693fb7e0d4ee48d1e779c
Abishek1912004 <21cse035.abishek@gmail.com>
2025-04-28 00:10:03 +0530

    in abishek add is done

---
+++ palindrome.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+class palindrome
+{
+    public static void main(String []args)
+    {
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+       multi(n);
+       
+    }
+
+    public static void multi(int n)
+    {
+        int c=n*n;
+       System.out.println(c);
+    }
+}
