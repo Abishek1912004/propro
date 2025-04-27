38641bcce995456d7fcc1a1e7700c1af306d3400
Abishek1912004 <21cse035.abishek@gmail.com>
2025-04-28 00:23:55 +0530

    abishek ,multiple

--- palindrome.java
+++ palindrome.java
@@ -1,48 +1,21 @@
+package propro;
+
+
 
 import java.util.*;
+
 class palindrome
 {
     public static void main(String []args)
     {
-       Scanner sc=new Scanner(System.in);
-       int n=sc.nextInt();
-       multi(n);
-       
-    }
-
-    public static void multi(int n)
-    {
-        int c=n*n;
-       System.out.println(c);
-    }
-
-    public static void add(int n)
-    {
-        int c=n+n;
-       System.out.println(c);
-    }import java.util.*;
-class Palindrome
-{
-    public static void main(String []args)
-    {
        Scanner sc = new Scanner(System.in);
-       System.out.print("Enter a number: ");
        int n = sc.nextInt();
-       sc.close();
-       multi(n);
-       add(n);
+       multi(n); // Now you can call it because both are static
     }
 
-    public static void multi(int n)
+    public static void multi(int n) // Add static here
     {
-        int result = n * n;
-        System.out.println("Square of " + n + " is: " + result);
+        int c = n * n;
+        System.out.println(c);
     }
-
-    public static void add(int n)
-    {
-        int result = n + n;
-        System.out.println("Double of " + n + " is: " + result);
-    }
-}
 }
