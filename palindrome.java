6dd61145b38c68c4cc9470b731c17bdc780d4def
Abishek1912004 <21cse035.abishek@gmail.com>
2025-04-28 00:28:19 +0530

    kanna

---
+++ palindrome.java
@@ -0,0 +1,18 @@
+package propro;
+import java.util.*;
+
+class palindrome
+{
+    public static void main(String []args)
+    {
+       Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+       add(n); 
+    }
+
+    public static void add(int n) 
+    {
+        int c = n + n;
+        System.out.println(c);
+    }
+}
